d03cb51c84c514b0015df1335dbb795bdafeb815
ShutovKS <schutow.kir@yandex.ru>
2024-05-10 12:13:14 +0300

    Add GameBootstrap

---
+++ src/Store simulation/Assets/!ProjectFiles/Features/Infrastructure/GameBootstrap.cs
@@ -0,0 +1,16 @@
+﻿using Infrastructure.ProjectStateMachine.Core;
+
+namespace Infrastructure
+{
+    public class GameBootstrap
+    {
+        public GameBootstrap()
+        {
+            StateMachine = new StateMachine<GameBootstrap>();
+
+            // StateMachine.SwitchState<>();
+        }
+
+        public readonly StateMachine<GameBootstrap> StateMachine;
+    }
+}
