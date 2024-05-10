fdad2ebad725d6e7660852f34251f43f9a1209b4
ShutovKS <schutow.kir@yandex.ru>
2024-05-10 12:31:22 +0300

    Add states

---
+++ src/Store simulation/Assets/!ProjectFiles/Features/Infrastructure/ProjectStateMachine/States/BootstrapState.cs
@@ -0,0 +1,20 @@
+﻿using Infrastructure.ProjectStateMachine.Core;
+using Zenject;
+
+namespace Infrastructure.ProjectStateMachine.States
+{
+    public class BootstrapState : IState<GameBootstrap>, IInitializable
+    {
+        public GameBootstrap Initializer { get; }
+
+        public BootstrapState(GameBootstrap initializer)
+        {
+            Initializer = initializer;
+        }
+
+        public void Initialize()
+        {
+            Initializer.StateMachine.SwitchState<InitializationState>();
+        }
+    }
+}
---
+++ src/Store simulation/Assets/!ProjectFiles/Features/Infrastructure/ProjectStateMachine/States/GameplayState.cs
@@ -0,0 +1,49 @@
+﻿using Infrastructure.ProjectStateMachine.Core;
+using Infrastructure.Services.AssetsAddressables;
+using Infrastructure.Services.WindowsService;
+using UnityEngine.AddressableAssets;
+
+namespace Infrastructure.ProjectStateMachine.States
+{
+    public class GameplayState : IState<GameBootstrap>, IEnterableWithArg<bool>, IExitable
+    {
+        public GameBootstrap Initializer { get; }
+        private readonly IWindowService _windowService;
+        private readonly IAssetsAddressablesProvider _assetsAddressablesProvider;
+
+        public GameplayState(GameBootstrap initializer,
+            IWindowService windowService,
+            IAssetsAddressablesProvider assetsAddressablesProvider)
+        {
+            Initializer = initializer;
+            _windowService = windowService;
+            _assetsAddressablesProvider = assetsAddressablesProvider;
+        }
+
+        public void OnEnter(bool isTest)
+        {
+            var sceneForLoad =
+                isTest ? AssetsAddressableConstants.FEATURE_TEST_SCENE : AssetsAddressableConstants.GAMEPLAY_SCENE;
+
+            var asyncOperation = Addressables.LoadSceneAsync(sceneForLoad);
+
+            asyncOperation.Completed += _ => { OpenGameplayWindow(); };
+        }
+
+        private async void OpenGameplayWindow()
+        {
+            await _windowService.Open(WindowID.Gameplay);
+
+            CloseLoadingWindow();
+        }
+
+        private void CloseLoadingWindow()
+        {
+            _windowService.Close(WindowID.Loading);
+        }
+
+        public void OnExit()
+        {
+        }
+    }
+}
