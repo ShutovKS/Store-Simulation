bfacc7404a4882b8bd2f286225dbdaec68f4ee50
ShutovKS <schutow.kir@yandex.ru>
2024-05-10 12:30:25 +0300

    Add Installers

--- src/Store simulation/Assets/!ProjectFiles/Features/Infrastructure/GameBootstrap.cs
+++ src/Store simulation/Assets/!ProjectFiles/Features/Infrastructure/GameBootstrap.cs
@@ -1,14 +1,23 @@
 ﻿using Infrastructure.ProjectStateMachine.Core;
+using Infrastructure.ProjectStateMachine.States;
+using Infrastructure.Services.AssetsAddressables;
+using Infrastructure.Services.WindowsService;
 
 namespace Infrastructure
 {
     public class GameBootstrap
     {
-        public GameBootstrap()
+        public GameBootstrap(IWindowService windowService,
+            IAssetsAddressablesProvider assetsAddressablesProvider)
         {
-            StateMachine = new StateMachine<GameBootstrap>();
-
-            // StateMachine.SwitchState<>();
+            StateMachine = new StateMachine<GameBootstrap>(new BootstrapState(this),
+                new InitializationState(this),
+                new ResourcesLoadingState(this, windowService),
+                new GameMainMenuState(this, windowService),
+                new GameplayState(this, windowService, assetsAddressablesProvider)
+            );
+            
+            StateMachine.SwitchState<BootstrapState>();
         }
 
         public readonly StateMachine<GameBootstrap> StateMachine;
---
+++ src/Store simulation/Assets/!ProjectFiles/Features/Infrastructure/Installers/ServiceInstaller.cs
@@ -0,0 +1,70 @@
+﻿using Infrastructure.Services.AssetsAddressables;
+using Infrastructure.Services.CoroutineRunner;
+using Infrastructure.Services.Factory.UIFactory;
+using Infrastructure.Services.Input;
+using Infrastructure.Services.Progress.Progress;
+using Infrastructure.Services.Progress.SaveLoad;
+using Infrastructure.Services.SoundsService;
+using Infrastructure.Services.WindowsService;
+using UnityEngine;
+using Zenject;
+
+namespace Infrastructure.Installers
+{
+    public class ServiceInstaller : MonoInstaller, ICoroutineRunner
+    {
+        [SerializeField] private PlayerInputActionReader playerInputActionReader;
+
+        public override void InstallBindings()
+        {
+            BindCurrentProgressService();
+            BindUIFactory();
+            BindAssetsAddressableService();
+            BindPlayerInputActionReader();
+            BindWindowService();
+            BindCoroutineRunnerService();
+            BindSaveLoadService();
+            BindSoundsService();
+        }
+
+        private void BindSoundsService()
+        {
+            Container.BindInterfacesTo<SoundService>().AsSingle();
+        }
+
+        private void BindSaveLoadService()
+        {
+            Container.BindInterfacesTo<SaveLoadService>().AsSingle();
+        }
+
+        private void BindCurrentProgressService()
+        {
+            Container.BindInterfacesTo<CurrentProgressService>().AsSingle();
+        }
+
+        private void BindWindowService()
+        {
+            Container.BindInterfacesTo<WindowService>().AsSingle();
+        }
+
+        private void BindAssetsAddressableService()
+        {
+            Container.BindInterfacesTo<AssetsAddressablesProvider>().AsSingle();
+        }
+
+        private void BindPlayerInputActionReader()
+        {
+            Container.Bind<PlayerInputActionReader>().FromInstance(playerInputActionReader).AsSingle();
+        }
+
+        private void BindUIFactory()
+        {
+            Container.BindInterfacesTo<UIFactory>().AsSingle();
+        }
+
+        private void BindCoroutineRunnerService()
+        {
+            Container.Bind<ICoroutineRunner>().FromInstance(this);
+        }
+    }
+}
